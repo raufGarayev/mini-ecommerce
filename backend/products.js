634655f810b58cdca1076cd8fe7d23169ca5a7a4
GARAYEV-PG <rafkagarayev@gmail.com>
2023-02-19 23:45:22 +0400

    first commit

---
+++ backend/products.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id: 1,
+        name: "Iphone 12 Pro",
+        desc: "6.1 inch display",
+        price: 999,
+        image: "https://res.cloudinary.com/dt56dmmc4/image/upload/v1676818130/onlineShop/iphone12pro_neiyzp.jpg"
+    },
+    {
+        id: 2,
+        name: "Iphone 12",
+        desc: "5.4 inch display",
+        price: 699,
+        image: "https://res.cloudinary.com/dt56dmmc4/image/upload/v1676818130/onlineShop/iphone12_ziljvo.jpg"
+    },
+    {
+        id: 3,
+        name: "Galaxy S20",
+        desc: "6.5 inch display",
+        price: 899,
+        image: "https://res.cloudinary.com/dt56dmmc4/image/upload/v1676818130/onlineShop/galaxyS_nxp1u0.png"
+    }
+];
+
+module.exports = products;
