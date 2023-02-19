c6de7d7139158e183851c7537bd90d0ede98acb9
GARAYEV-PG <rafkagarayev@gmail.com>
2023-02-20 01:58:25 +0400

    backend improved

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const cors = require('cors')
+
+const mongoose = require("mongoose")
+const register = require("./routes/register")
+const login = require("./routes/login")
 const products = require("./products")
+
 const app = express()
+
+require("dotenv").config()
+
 
 app.use(express.json())
 app.use(cors())
+
+app.use("/api/register", register)
+app.use("/api/login", login)
 
 app.get("/", (req, res) => {
     res.send("Welcome to our online shop API...")
@@ -15,5 +26,12 @@ app.get("/products", (req, res) => {
 })
 
 const port = process.env.PORT || 5000
+const uri = process.env.DB_URL
 
 app.listen(port, console.log(`Server running on port ${port}`))
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("mongodb connection successfull"))
+.catch(() => console.log("mongodb connection failed", err.message))
